676ac09bc4f0bf2e6879d5733b84413b42e66427
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-02-08 12:16:15 +0000

    merging task 10

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Login() {
-  return <h1>Log-in Page</h1>;
+  return (
+    <section className="login-page">
+      <h1>Log-in Page</h1>
+    </section>
+  );
 }
 
 export default Login;
